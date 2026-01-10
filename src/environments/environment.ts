216759d2d4d65cdb5e27ab8b1cffb186a3482f70
ravirajvaidya <ravirajvaidya06@gmail.com>
2026-01-10 13:47:53 +0530

    Removed unwanted files

--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    supabaseUrl: 'https://mukbrnrqnjreciwfubtv.supabase.co',
-    supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im11a2JybnJxbmpyZWNpd2Z1YnR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc4NTQ5MDgsImV4cCI6MjA4MzQzMDkwOH0.WUe_x_T8ZqLKJi-g84krfUi-laq5cJ26cu5DHJNKyfE'
-};
